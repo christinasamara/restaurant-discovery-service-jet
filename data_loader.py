5c7cebff31c9157b8adf710ec5ed34b0d66bf29e
christinasamara <christinaesamara@gmail.com>
2026-04-08 10:28:49 +0300

    Add data loader to fetch and clean restaurant information by postcode

---
+++ data_loader.py
@@ -0,0 +1,44 @@
+import requests
+
+def load_data(postcode="EC4M7RF"):
+    url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
+    
+    try:
+        headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status() 
+        full_data = response.json()
+        raw_restaurants = full_data.get('restaurants', [])[:10]
+
+        restaurants = []
+        for res in raw_restaurants:
+            cuisine_list = [c.get('name') for c in res.get('cuisines', [])]
+            addr = res.get('address', {})
+            full_address = f"{addr.get('firstLine')}, {addr.get('city')}, {addr.get('postalCode')}"
+
+            restaurants.append({
+                "name": res.get('name'),
+                "cuisines": ", ".join(cuisine_list),
+                "rating": res.get('rating', {}).get('starRating'),
+                "address": full_address
+            })
+
+        return restaurants
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return []
+
+if __name__ == "__main__":
+    data = load_data("EC4M7RF")
+    if data:
+        print(f"Fetched {len(data)} restaurants.")
+
+        print(f"all data: {data}")
+        
+        print(f"First Restaurant: {data[2].get('name')}")
+        print(f"First Restaurant Cuisine: {data[2].get('cuisines')}")
+        print(f"First Restaurant Rating: {data[2].get('rating')}")
+        print(f"First Restaurant Address: {data[2].get('address')}")
